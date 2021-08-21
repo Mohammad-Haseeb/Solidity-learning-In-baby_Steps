59e6c2e978e7ceb41807636ecdf476961ff41ee4
Muhammad Haseeb Hussain <64950545+Mohammad-Haseeb@users.noreply.github.com>
2021-08-21 12:05:15 +0500

    rule4.sol

---
+++ Variables-Rules/Rule4.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+contract Rule4{
+   
+   
+//   Arugments supplied by caller to function are always stored in calldata data location
+   
+//   like add(3,5); now these argument are stored in calldata
+     
+    
+}
