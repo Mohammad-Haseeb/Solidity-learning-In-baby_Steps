c145be74b0e76008be8a9f2f3392d766a7b20cdc
Muhammad Haseeb Hussain <64950545+Mohammad-Haseeb@users.noreply.github.com>
2021-08-22 01:45:44 +0500

    cryptoglobalVariables

---
+++ Global-Variables/globaCryptoVariables.sol
@@ -0,0 +1,11 @@
+
+pragma solidity 0.8.7;
+
+contract GlobalCryptoVariables{
+
+         function hashGenerator() public returns(bytes32){
+            //  keccak256("Eg"); example
+              return sha256("256");
+         }
+    
+}
