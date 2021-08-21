f73fd7e8e121b1fc7ec0cfc816034480db0079f2
Muhammad Haseeb Hussain <64950545+Mohammad-Haseeb@users.noreply.github.com>
2021-08-21 10:37:08 +0500

    rule1

---
+++ Rule.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+contract Rule1{
+    int256 public ab=12;
+    // state variables  are always stored in storage data location
+     
+    
+}
